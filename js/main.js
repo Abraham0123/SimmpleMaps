2da2bf1e640c1cee36540d11c80d2338852bbc8a
Riaz <m_riaz@tss-hr420-dm19.fc.ca>
2017-03-03 15:56:51 -0500

    adding directions, geocoding, drawing on the map

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 (function() {
-	var map, marker;
+	var map = new google.maps.Map(document.querySelector('.wrapper')), marker;
 
-	function initMap() {
-		map = new google.maps.Map(document.querySelector('.map-wrapper'), {
-			center : { lat: 42.983233, lng: -81.250688 },
-			zoom : 14
-		});
+	function initMap(position) {
+			map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude });
+			map.setZoom(14);
 
-		marker = new google.maps.Marker({
-			position: { lat: 42.983233, lng: -81.250688 },
+			marker = new google.maps.Marker({
+			position: { lat: position.coords.latitude, lng: position.coords.longitude },
 			map: map,
 			title: "Hello World!"
 		});
 	}
 
-	initMap();
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(initMap, handleError);
+
+	} else {
+		console.log('we were on a break!');
+	}
+
+	function handleError() {
+		console.log('something went wrong');
+	}
+
 })();
